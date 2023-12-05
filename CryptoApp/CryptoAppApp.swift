5b6414e75264ae8ebb93248fe52ef95990fc961d
Tharusha <tharushalakshan3@gmail.com>
2023-12-05 23:07:24 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Tharusha Lakshan on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoApp/Extentions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Tharusha Lakshan on 12/5/23.
+//
+
+import Foundation
