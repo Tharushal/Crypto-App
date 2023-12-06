8c41dbc47fbe17fb666199da814ac698116f1326
Tharusha <tharushalakshan3@gmail.com>
2023-12-06 14:25:23 +0530

    creating HomeView & Navigation Header

---
+++ CryptoApp/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonAnimationView.swift
+//  CryptoApp
+//
+//  Created by Tharusha Lakshan on 12/6/23.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView()
+}
---
+++ CryptoApp/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  CryptoApp
+//
+//  Created by Tharusha Lakshan on 12/6/23.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
