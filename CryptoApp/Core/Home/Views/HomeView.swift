5e2fba61b30fec162e8900a25008f8a5d47f60aa
Tharusha <tharushalakshan3@gmail.com>
2023-12-06 14:28:27 +0530

    homeview & navigation header

--- CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            //background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            VStack{
+                
+                homeHeader
+               
+                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
+
+
+
+
+
+
+extension HomeView{
+    
+    private var homeHeader: some View{
+        HStack{
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                CircleButtonAnimationView(animate: $showPortfolio))
+            Spacer()
+            Text(showPortfolio ? "Portfolio":"Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture{
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
+
+
+
+
+
+
+
+
 #Preview {
-    HomeView()
+    
+        HomeView()
+    
 }
